1a59b5171a69b737bf60cd27622db8d890f4eb82
ezgi özgen <ozgenn.ezgi@gmail.com>
2021-03-22 21:37:54 +0300

    20 Mart

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -74,7 +74,9 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseCors(builder=>builder.WithOrigins("http://localhost:53836").AllowAnyHeader());
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
